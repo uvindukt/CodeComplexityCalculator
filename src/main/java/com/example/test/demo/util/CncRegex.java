89645ddf81ca70dc2dfbaebb8199106ec4502798
Uvindu Sanjana <zenj86@gmail.com>
2019-08-26 17:27:51 +0530

    updated

--- src/main/java/com/example/test/demo/util/CncRegex.java
+++ src/main/java/com/example/test/demo/util/CncRegex.java
@@ -2,7 +2,7 @@ package com.example.test.demo.util;
 
 public class CncRegex {
 
-    public static final String REGEX_OPEN = "if.+?\\{|else.+?\\{|for.+?\\{|while.+?\\{";
+    public static final String REGEX_OPEN = "\\W+if.+?\\{|else.+?\\{|for.+?\\{|while.+?\\{";
     public static final String REGEX_CLOSE = "}";
 
 }
